4d56eb081e42b7e301792e4c097fd95e98f8026f
PIXDEX <elcyclez1@gmail.com>
2026-06-08 12:38:28 +0000

    Remove console log and add alert

--- script.js
+++ script.js
@@ -1,5 +1,1 @@
-console.log("Bienvenido a la web de Pethon;");
-
-document.querySelector('.boton-git').addEventListener('click', () => {
-    alert("Estás yendo al repositorio de Pethon;");
-});
+alert("LoL");
